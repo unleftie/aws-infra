5d0732a73973dd581491295f102005a459739cbf
unleftie <denysrondaliev@gmail.com>
2024-10-03 22:49:08 +0300

    fix: added helm provider

--- argocd/provider.tf
+++ argocd/provider.tf
@@ -11,3 +11,11 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.this.token
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
 }
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.this.endpoint
+    token                  = data.aws_eks_cluster_auth.this.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  }
+}
